3a4d3b44b2786e1e6b07d37f008a8374168fb4d7
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-08-17 16:08:00 +0900

    SettingPageの更新

--- lib/views/pages/setting/setting_page.dart
+++ lib/views/pages/setting/setting_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import "./components/record_card.dart";
 import "./components/custom-header.dart";
+import "./components/item.dart";
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
 
@@ -11,6 +12,8 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final containerHeight = screenHeight / 3;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
@@ -19,9 +22,88 @@ class _SettingPageState extends State<SettingPage> {
         children: [
           CustomHeader(title: 'あなたについて'),
           RecordCard(),
+          Container(
+        height: containerHeight,
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '設定',
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.notifications_active,
+                ),
+                Text(
+                  '通知をON',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SwitchItem(),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.stay_primary_portrait,
+                ),
+                Text(
+                  '入出金をデフォルトにする',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SwitchItem(),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.info,
+                ),
+                Text(
+                  'このアプリについて',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.send,
+                ),
+                Text(
+                  'フィードバックをおくる',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+          
+          
+          
+          
         ],
       ),
-    ),
+      ),
     );
   }
 }
