784f87cb8648ccfd4316b4d4ad4043f298b03a47
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-08-19 16:59:45 +0900

    UserLogNotifierの作成

---
+++ lib/views/pages/home/providers/user_log.dart
@@ -0,0 +1,21 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'user_log.g.dart';
+
+@riverpod
+class UserLogNotifier extends _$UserLogNotifier {
+  // 初期状態として空のリストを返す
+  @override
+  List<Map<String, dynamic>> build() {
+    return [];
+  }
+
+  // データを追加する関数
+  void updateState(Map<String, dynamic> newData) {
+    // 現在のデータをコピーして新しいデータを追加
+    final oldState = state;
+    final newState = [...oldState, newData];
+    state = newState; // 状態を更新
+    print("aaanewState: $newState");
+  }
+}
---
+++ lib/views/pages/home/providers/user_log.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_log.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userLogNotifierHash() => r'5f1b50e882e156513b7e02aa6f4ad684306b078a';
+
+/// See also [UserLogNotifier].
+@ProviderFor(UserLogNotifier)
+final userLogNotifierProvider = AutoDisposeNotifierProvider<UserLogNotifier,
+    List<Map<String, dynamic>>>.internal(
+  UserLogNotifier.new,
+  name: r'userLogNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userLogNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserLogNotifier = AutoDisposeNotifier<List<Map<String, dynamic>>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
