a7e0992b7ba5d76e63a91b1b056bf0bca9eee291
Shvain <takuto3987@gmail.com>
2024-08-23 17:23:33 +0900

    履歴を削除した場合の関数を追加

--- lib/views/pages/home/providers/user_log.dart
+++ lib/views/pages/home/providers/user_log.dart
@@ -22,4 +22,25 @@ class UserLogNotifier extends _$UserLogNotifier {
     state = newState; // 状態を更新
     print("newState: $newState");
   }
+
+// データを削除する関数
+void deleteLog(int index) {
+  if (index >= 0 && index < state.length) {
+    // インデックスがリストの範囲内であることを確認する
+    final newState = List<Map<String, dynamic>>.from(state);
+    newState.removeAt(index); // 指定したインデックスの項目を削除
+    state = newState; // 更新された状態を反映
+  } else {
+    // 範囲外のインデックスに対する処理（エラーハンドリング）
+    print('Invalid index: $index');
+  }
+}
+
+  // データを元に戻す関数
+  void undoDelete(int index, Map<String, dynamic> item) {
+    final oldState = state;
+    final newState = List<Map<String, dynamic>>.from(oldState);
+    newState.insert(index, item);
+    state = newState;
+  }
 }
