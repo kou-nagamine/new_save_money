d47d446487ce3c68b478d13d1d4ffac25016776d
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-08-30 14:35:02 +0900

    リバーポッドの更新

--- lib/views/pages/calculator/providers/all_price.dart
+++ lib/views/pages/calculator/providers/all_price.dart
@@ -6,19 +6,20 @@ part 'all_price.g.dart';
 @Riverpod(keepAlive: true)
 class AllPriceNotifier extends _$AllPriceNotifier{
   @override
-  int build() {
-    return 0;
+  List<int> build() {
+    return [0,0]; //[我慢合計額, 支払い額込み金額]
   }
 
   void updateAllPrice(int price) {
     if(price != 0){
-      state += price;
+      state[0] += price;
+      state[1] += price;
     } 
   }
 
   void subtractPrice(int price) {
     if (price != 0) {
-      state -= price;
+      state[1] -= price;
     }
   }
 }
