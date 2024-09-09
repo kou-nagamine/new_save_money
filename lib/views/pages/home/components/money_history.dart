18a06b98cd10f3f6cfd1a9e82fef70b485f43294
Shvain <takuto3987@gmail.com>
2024-09-09 13:39:30 +0900

    feat：ホームページのリデザイン

--- lib/views/pages/home/components/money_history.dart
+++ lib/views/pages/home/components/money_history.dart
@@ -1,6 +1,8 @@
+import 'package:figma_squircle/figma_squircle.dart';
 import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:iconoir_flutter/iconoir_flutter.dart'as iconoir;
 import '../providers/user_log.dart';
 import 'package:new_save_money/views/pages/calculator/providers/all_price.dart';
 import 'package:intl/intl.dart'; // NumberFormatを使用するためにインポート
@@ -17,7 +19,7 @@ class MoneyHistoryList extends ConsumerWidget {
     final firstDepositFalseIndex = userSaveLog.indexWhere((item) => !item.deposit);
 
     return ListView.builder(
-      padding: const EdgeInsets.only(top: 10),
+      padding: const EdgeInsets.only(top: 0),
       itemCount: userSaveLog.length, // 履歴データの数だけリストアイテムを作成
       itemBuilder: (context, index) {
         final item = userSaveLog[index]; // 各アイテムのデータを取得
@@ -30,7 +32,7 @@ class MoneyHistoryList extends ConsumerWidget {
         final int price = item.price;
 
         // 用途に応じて色を変更
-        final Color priceColor = deposit ? Color(0xFF2CB13C) : Color(0xFFE82929);
+        final Color priceColor = deposit ? Colors.black : Color(0xFFE82929);
 
         // Dismissibleで囲む条件: 最新の deposit = false のアイテム以前のもの、または deposit = false がない場合
         final shouldWrapWithDismissible = firstDepositFalseIndex == -1 || index <= firstDepositFalseIndex;
@@ -62,51 +64,92 @@ class MoneyHistoryList extends ConsumerWidget {
   }
 
   // 共通のListTileビルダー
-  ListTile buildListTile(BuildContext context, String categoryName, IconData categoryIcon, Color color, int price, Color priceColor, bool deposit) {
-    return ListTile(
-      contentPadding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0.0),
-      title: Text(
-        categoryName,
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-          color: Theme.of(context).brightness == Brightness.dark
-              ? Colors.white
-              : Colors.black87,
-        ),
+  Widget buildListTile(BuildContext context, String categoryName, IconData categoryIcon, Color color, int price, Color priceColor, bool deposit) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      padding: const EdgeInsets.all(0),
+      decoration: ShapeDecoration(
+        //color: Color(0xffD9D9D9),
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Color(0xffDDDDDD),
+            width: 1.5,
+          ),
+          borderRadius: SmoothBorderRadius(
+            cornerRadius: 20,
+            cornerSmoothing: 0.7,
+          ),
+        )
       ),
-      leading: Container(
-        width: 40,
-        height: 40,
-        decoration: BoxDecoration(
-          color: Colors.grey[200],
-          shape: BoxShape.circle,
+      child: ListTile(
+        dense: true,
+        contentPadding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+        title: Text(
+          categoryName,
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Color(0xff3C3C43),
+          ),
         ),
-        child: Icon(
-          categoryIcon,
-          size: 20,
-          color: color,
+        subtitle: Text(
+          "2021/10/10",
+          style: TextStyle(
+            fontSize: 13,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Color(0xffA4A4A4),
+          ),
         ),
-      ),
-      trailing: Text(
-        "¥${NumberFormat("#,###").format(price)}",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: priceColor, // ここで色を設定
+        leading: Container(
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(
+            color: Color(0xffF1F1F1),
+            shape: BoxShape.circle,
+          ),
+          child: Icon(
+            categoryIcon,
+            size: 20,
+            color: color,
+          ),
         ),
-      ),
-      onTap: () {
-        // depositがfalseの場合のみReferencePageに遷移
-        if (!deposit) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => ReferencePage(),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min, // trailing内のアイテムの横幅を最小限にする
+          children: [
+            Text(
+              "¥${NumberFormat("#,###").format(price)}",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+                color: priceColor,
+              ),
             ),
-          );
-        }
-      },
+            Opacity(
+            opacity: deposit ? 0.0 : 1.0, // depositがtrueなら透明に
+            child: Icon(
+              Icons.chevron_right_rounded,
+              size: 30,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+        onTap: () {
+          // depositがfalseの場合のみReferencePageに遷移
+          if (!deposit) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ReferencePage(),
+              ),
+            );
+          }
+        },
+      )
     );
   }
 }
