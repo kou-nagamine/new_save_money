7f93bdd0b7d411975d1bef3c64367f07ac8290f4
Shvain <takuto3987@gmail.com>
2024-09-10 17:21:20 +0900

    fix：グラフのデータを同期

--- lib/views/pages/home/components/category_graph.dart
+++ lib/views/pages/home/components/category_graph.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:figma_squircle/figma_squircle.dart';
 
-class CategoryBarChart extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//riverpods
+import 'package:new_save_money/views/pages/calculator/providers/all_price.dart';
+import 'package:new_save_money/views/pages/home/providers/user_log.dart';
+
+class CategoryBarChart extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final allPrice = ref.watch(allPriceNotifierProvider); 
+    final userData = ref.watch(userLogNotifierProvider);
+    final Map<String, Map<String, dynamic>> categoryData = {};
+
+      // userData の各要素に対して処理を行う
+    for (var save in userData) {
+      if (categoryData.containsKey(save.name)) {
+        categoryData[save.name]!['totalPrice'] += save.price;
+      } else {
+        categoryData[save.name] = {
+          'totalPrice': save.price,
+        };
+      }
+    }
+    // allPrice[1] が存在し、かつ 0 でないことを確認して、ゼロ割りを回避する
+    double totalAmount = (allPrice.length > 1 && allPrice[0] != 0)
+    ? allPrice[0].toDouble()
+    : 1.0; // allPrice[1] が 0 または存在しない場合は 1.0 を使用
+
     // それぞれのコンテナの幅の割合
-    double drinkPercentage = 0.67; // 67%
-    double foodPercentage = 0.20;  // 20%
-    double snackPercentage = 0.10; // 10%
-    double otherPercentage = 0.03; // 3%
+    double drinkPercentage = ((categoryData['飲み物']?['totalPrice'] ?? 0 ) / totalAmount).toDouble();
+    double foodPercentage = ((categoryData['食事']?['totalPrice'] ?? 0 ) / totalAmount).toDouble();
+    double snackPercentage = ((categoryData['菓子類']?['totalPrice'] ?? 0 ) / totalAmount).toDouble();
+    double otherPercentage = ((categoryData['その他']?['totalPrice'] ?? 0 ) / totalAmount).toDouble();
 
     // 画面の横幅を取得
     double screenWidth = MediaQuery.of(context).size.width * 0.83;
@@ -20,9 +45,33 @@ class CategoryBarChart extends StatelessWidget {
     double otherWidth = screenWidth * otherPercentage;
 
     // テキストを表示するための最小幅
-    double minTextWidth = 40;
+    double minTextWidth = 70;
 
-    return Row(
+    return allPrice[1] == 0
+      ? Container(
+        decoration: ShapeDecoration(
+          shape: SmoothRectangleBorder(
+            side: BorderSide(
+              color: Color(0xffDADADA),
+              width: 1,
+            ),
+            borderRadius: SmoothBorderRadius(
+              cornerRadius: 5,
+              cornerSmoothing: 0.6),
+          ),
+        ),
+        height: 50,
+        width: screenWidth,
+        alignment: Alignment.center,
+        child: Text(
+          'データがないためグラフを表示できません',
+          style: TextStyle(
+            fontSize: 13,
+            color: Colors.black,
+          ),
+        ),
+      ) 
+      : Row(
       children: [
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,11 +89,20 @@ class CategoryBarChart extends StatelessWidget {
                 height: 50,
               ),
             ),
-            Text(
-              '飲み物',
-              style: TextStyle(
-                fontSize: 10,
-                color: Color(0xff5B5B5B),
+            if (drinkWidth >= minTextWidth) // コンテナの幅がminTextWidth以上の場合のみ表示
+              Text(
+                '飲み物 ${(drinkPercentage * 100).toStringAsFixed(1)}%',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xff5B5B5B),
+              ),
+            )
+            else
+              Text(
+                '...',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xff5B5B5B),
               ),
             )
           ],
@@ -66,11 +124,20 @@ class CategoryBarChart extends StatelessWidget {
                 height: 50,
               ),
             ),
-            Text(
-              '飲み物',
-              style: TextStyle(
-                fontSize: 10,
-                color: Color(0xff5B5B5B),
+            if (foodWidth >= minTextWidth) // コンテナの幅がminTextWidth以上の場合のみ表示
+              Text(
+                '食事 ${(foodPercentage * 100).toStringAsFixed(1)}%',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xff5B5B5B),
+              ),
+            )
+            else
+              Text(
+                '...',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xff5B5B5B),
               ),
             )
           ],
@@ -92,11 +159,20 @@ class CategoryBarChart extends StatelessWidget {
                 height: 50,
               ),
             ),
-            Text(
-              '飲み物',
-              style: TextStyle(
-                fontSize: 10,
-                color: Color(0xff5B5B5B),
+            if (snackWidth >= minTextWidth) // コンテナの幅がminTextWidth以上の場合のみ表示
+              Text(
+                '菓子類 ${(snackPercentage * 100).toStringAsFixed(1)}%',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xff5B5B5B),
+              ),
+            )
+            else
+              Text(
+                '...',
+                style: TextStyle(
+                  fontSize: 10,
+                  color: Color(0xff5B5B5B),
               ),
             )
           ],
@@ -120,7 +196,7 @@ class CategoryBarChart extends StatelessWidget {
             ),
             if (otherWidth >= minTextWidth) // コンテナの幅がminTextWidth以上の場合のみ表示
               Text(
-                'その他',
+                'その他 ${(otherPercentage * 100).toStringAsFixed(1)}%',
                 style: TextStyle(
                   fontSize: 10,
                   color: Color(0xff5B5B5B),
@@ -128,7 +204,7 @@ class CategoryBarChart extends StatelessWidget {
             )
             else
               Text(
-                '',
+                '...',
                 style: TextStyle(
                   fontSize: 10,
                   color: Color(0xff5B5B5B),
