e50022cf79feb7ddff0da8e04a9eda6a3d057629
HIMA-HIMAZIN <taiyaki850@gmail.com>
2024-09-25 16:40:20 +0900

    homeページでOSの戻る機能の制限

--- lib/views/pages/home/home_page.dart
+++ lib/views/pages/home/home_page.dart
@@ -25,166 +25,169 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context , WidgetRef ref) {
     final allPrice = ref.watch(allPriceNotifierProvider);
     final historyData = ref.watch(userLogNotifierProvider);//sharedPrefarence導入前監視用
-    return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Color(0xFFE3EEFF), Color(0xFFFFFFFF)],
-                begin: Alignment.topLeft,
-                end: Alignment(0.3, -0.2),
-                stops: [0.3, 0.8],
-              ),
-            ),
-            constraints: const BoxConstraints.expand(),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.08,
-            right: 30,
-            child: IconButton(
-              icon: const Icon(
-                Icons.menu,
-                size: 35,
-                color: Colors.black,
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/settingPage');
-              },
-            ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.13,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => GraphPage()),
-                );
-              },
-              child:  Container(
-                width: MediaQuery.of(context).size.width * 1.0,
-                padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'あなたの口座残高',
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xff5B5B5B),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          '¥ ${NumberFormat("#,###").format(allPrice[1])}',
-                          style: TextStyle(
-                            fontSize: 40,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
-                          ),
-                        ),
-                        //SizedBox(width: 50),
-                        Container(
-                          padding: EdgeInsets.only(right: 30),
-                          height: 26,
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                               Text(
-                                '詳細',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.blue,
-                                ),
-                              ),
-                              Icon(
-                                Icons.chevron_right,
-                                size: 30,
-                                color: Colors.blue,
-                              ),
-                            ],
-                          )
-                        )
-                      ]
-                    ),
-                    SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        RichText(
-                          text: TextSpan(
-                            text: '今までのついで合計額   ',
-                            style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xff5B5B5B),
-                            ),
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: '¥${NumberFormat("#,###").format(allPrice[0])}',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    SizedBox(height: 50,
-                    child: CategoryBarChart(),),
-                  ],
+    return PopScope(
+      canPop: false,
+      child:Scaffold(
+        body: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFE3EEFF), Color(0xFFFFFFFF)],
+                  begin: Alignment.topLeft,
+                  end: Alignment(0.3, -0.2),
+                  stops: [0.3, 0.8],
                 ),
-              )
+              ),
+              constraints: const BoxConstraints.expand(),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            child: Container(
-              width: MediaQuery.of(context).size.width * 1.0,
-              height: MediaQuery.of(context).size.height * 0.68,
-              padding: EdgeInsets.fromLTRB(25, 15, 25, 100),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.baseline, // ベースラインに揃える
-                    textBaseline: TextBaseline.alphabetic, // テキストのベースラインを使う
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.08,
+              right: 30,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  size: 35,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/settingPage');
+                },
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.13,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GraphPage()),
+                  );
+                },
+                child:  Container(
+                  width: MediaQuery.of(context).size.width * 1.0,
+                  padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       const Text(
-                        '履歴',
+                        'あなたの口座残高',
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 13,
                           fontWeight: FontWeight.bold,
+                          color: Color(0xff5B5B5B),
                         ),
                       ),
-                      PaymentMenu(),
-                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            '¥ ${NumberFormat("#,###").format(allPrice[1])}',
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                          ),
+                          //SizedBox(width: 50),
+                          Container(
+                            padding: EdgeInsets.only(right: 30),
+                            height: 26,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  '詳細',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                                Icon(
+                                  Icons.chevron_right,
+                                  size: 30,
+                                  color: Colors.blue,
+                                ),
+                              ],
+                            )
+                          )
+                        ]
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              text: '今までのついで合計額   ',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff5B5B5B),
+                              ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: '¥${NumberFormat("#,###").format(allPrice[0])}',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 5),
+                      SizedBox(height: 50,
+                      child: CategoryBarChart(),),
                     ],
                   ),
-                  Expanded(
-                    child: historyData.isEmpty
-                    ? const Padding(
-                      padding:  EdgeInsets.only(top: 50),
-                      child: Text('ついで出費を記録しよう！'))
-                    : MoneyHistoryList(),
-                  )
-                ],
+                )
               ),
             ),
-          ),
-        ],
-      ),
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width * 1.0,
+                height: MediaQuery.of(context).size.height * 0.68,
+                padding: EdgeInsets.fromLTRB(25, 15, 25, 100),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.baseline, // ベースラインに揃える
+                      textBaseline: TextBaseline.alphabetic, // テキストのベースラインを使う
+                      children: [
+                        const Text(
+                          '履歴',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        PaymentMenu(),
+                        Spacer(),
+                      ],
+                    ),
+                    Expanded(
+                      child: historyData.isEmpty
+                      ? const Padding(
+                        padding:  EdgeInsets.only(top: 50),
+                        child: Text('ついで出費を記録しよう！'))
+                      : MoneyHistoryList(),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
