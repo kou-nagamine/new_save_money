90d41118c810494d69b73027a446f7a87047b5e5
Shvain <takuto3987@gmail.com>
2024-09-25 22:52:56 +0900

    fix：databaseのインサートを編集

--- lib/firebase_insert.dart
+++ lib/firebase_insert.dart
@@ -6,33 +6,51 @@ class  DataInsert{
   Future<void> insertData() async {
     await _firestore.collection('topic_content').add({
       'category': '勉強',
-      'title': 'サブスク',
-      'description': '定期的な支払いに割り当てよう',
-      'image_url': 'study/copilot.jpeg',
+      'title': 'オンライン講座',
+      'description': '勉強をしよう',
+      'image_url': 'study/online.png',
     });
      await _firestore.collection('topic_content').add({
       'category': '勉強',
-      'title': 'デバイス',
-      'description': '新しいデバイスで学習効率を上げよう',
-      'image_url': 'study/macbook.jpeg',
+      'title': '書籍',
+      'description': '勉強するための本を読もう',
+      'image_url': 'study/frontend_book.jpg',
     });
      await _firestore.collection('topic_content').add({
       'category': '楽しむ',
-      'title': 'テーマパーク',
-      'description': '楽しい思い出を作ろう',
-      'image_url': 'enjoy/amusement_park.jpeg',
+      'title': '美容',
+      'description': 'コスメ、ヘアカラー、ネイルなど',
+      'image_url': 'enjoy/butiful.jpg',
     });
      await _firestore.collection('topic_content').add({
       'category': '楽しむ',
-      'title': '旅行',
-      'description': '新しい場所を探検しよう',
-      'image_url': 'enjoy/travel.webp',
+      'title': '外食',
+      'description': 'ディナー、飲み会など',
+      'image_url': 'enjoy/dinner.jpg',
     });
-     await _firestore.collection('topic_content').add({
+    await _firestore.collection('topic_content').add({
+      'category': '楽しむ',
+      'title': 'レジャー',
+      'description': '旅行、アクティビティなど',
+      'image_url': 'enjoy/leisure.jpg',
+    });
+    await _firestore.collection('topic_content').add({
+      'category': '楽しむ',
+      'title': 'SNS',
+      'description': 'インスタ、ツイッターなど',
+      'image_url': 'enjoy/sns.jpg',
+    });
+    await _firestore.collection('topic_content').add({
       'category': 'その他',
-      'title': '募金',
-      'description': '社会貢献をしよう',
-      'image_url': 'enjoy/Donation.png',
+      'title': 'ギフト',
+      'description': 'プレゼントを贈ろう',
+      'image_url': 'enjoy/gift.png',
+    });
+    await _firestore.collection('topic_content').add({
+      'category': 'その他',
+      'title': '生活費',
+      'description': '食費、日用品など',
+      'image_url': 'enjoy/life.jpg',
     });
     // 完了メッセージ
     print('データの挿入が完了しました');
